545308ac13b926da7fc1e0e08f0dbfb7bdee816b
meonwax <meonwax@meonwax.de>
2016-08-22 23:10:30 +0200

    Fix type conversion

--- src/cave/cavetypes.cpp
+++ src/cave/cavetypes.cpp
@@ -402,10 +402,11 @@ bool read_from_string(const std::string &s, GdInt &i) {
     std::istringstream is(s);
     /* was saved as a normal int */
     int read;
-    bool success = (is >> read);
-    if (success)
+    if (is >> read) {
         i = read;
-    return success;
+        return true;
+    }
+    return false;
 }
 
 
